9af1e0945c668667bde11830de0f9f81c163f8a6
KellyMenzul <kmenzul@my.bcit.ca>
2022-02-17 21:57:52 -0800

    Implement glider information

--- pages/glider.js
+++ pages/glider.js
@@ -2,8 +2,12 @@ import styled from 'styled-components'
 import { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
+import Card from '../comps/Card'
 
 const Container = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
 `
 
 export default function Glider() {
@@ -19,9 +23,28 @@ export default function Glider() {
   }, []);
 
   return ( <Container>
-    {data.map((o,i)=><div key={i}>
-      {o.Body}
-    </div>)}
+    {data.map((o,i)=>
+      <Card
+        key={i}
+        bgcolor={o.Color}
+
+
+        //front card
+        title='Glider'
+        name={o.Body}
+        img={o.Image}
+        height={'94.72'}
+        width={'148px'}
+
+        //back card
+        cat1={'Type'}
+        val1={o.Type}
+        cat2={'Weight'}
+        val2={o.Weight}
+        cat3={'Air Speed'}
+        val3={o["Speed (Air)"]}
+      />
+    )}
   </Container>
   )
 }
