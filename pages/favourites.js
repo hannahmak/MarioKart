0be2c6a65b471341d92653341524e8c9725d6dc0
Hannah MacPherson <hannahmacpherson@Hannahs-MacBook-Pro.local>
2022-02-20 23:27:24 -0800

    vercel

--- pages/favourites.js
+++ pages/favourites.js
@@ -15,64 +15,62 @@ import FilterButton from '../comps/FilterButton'
 import SortButton from '../comps/SortButton';
 
 const Container = styled.div`
-display: flex;
-flex-wrap: wrap;
-flex-direction:column;
-justify-content: center;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction:column;
+  justify-content: center;
 `
 
 const TopBarCont = styled.div`
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-width:100%;
-position:fixed;
-height:10%;
-background-color:#FB0A40;
-z-index:20;
-top:0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  position: fixed;
+  height: 10%;
+  background-color: #FB0A40;
+  z-index: 20;
+  top: 0px;
 `
 
 const CardCont = styled.div`
-height:70%;
-width:100%;
-display: flex;
-flex-wrap: wrap;
-justify-content: center;
-margin-bottom:100px;
+  height: 70%;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 100px;
 `
 
 const HeadingCont = styled.div`
-display:flex;
-flex-direction:column;
-width:100%;
-align-items:center;
-justify-content:center;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
 `
 
 const BottomBarCont = styled.div `
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-width:100%;
-position:fixed;
-height:10%;
-background-color:#FB0A40;
-z-index:20;
-bottom:0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  position: fixed;
+  height: 10%;
+  background-color: #FB0A40;
+  z-index: 20;
+  bottom: 0px;
 `
 const Header = styled.h2`
-display:flex;
-flex-direction:column;
-justify-content:center;
-align-items:center;
-width:100%;
-margin-top:100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 100px;
 `
-
-
 
 export default function Fav() {
   const {favC, setFavC} = useFavC();
@@ -83,108 +81,115 @@ export default function Fav() {
 
   return (
     <Container>
-    <TopBarCont>
-      <TopMenu link='./'/>
-    </TopBarCont>
-    <HeadingCont>
-      <Header>Favourites</Header>
-    </HeadingCont>
-      <CardCont>
-      {Object.values(favC).map((o,i)=>
-      <Card
-          
-      //card stylying
-      name={o.Character}
-      bgcolor={o.Color}
-      img={o.Image}
-      height={'114px'}
-      width={'120px'}
+      <TopBarCont>
+        <TopMenu link='./'/>
+      </TopBarCont>
+      <HeadingCont>
+        <Header>Favourites</Header>
+      </HeadingCont>
+        <CardCont>
+          {Object.values(favC).map((o,i)=> 
+          <div key={i}>
+            <Card
+            //card stylying
+            name={o.Character}
+            bgcolor={o.Color}
+            img={o.Image}
+            height={'114px'}
+            width={'120px'}
 
-      //stats info
-      cat1={'Weight'}
-      val1={o.Weight}
-      
-      cat2={'Acceleration'}
-      val2={o.Acceleration}
+            //stats info
+            cat1={'Weight'}
+            val1={o.Weight}
 
-      cat3={''}
-      val3={null}
-    >
-    </Card>
+            cat2={'Acceleration'}
+            val2={o.Acceleration}
+
+            cat3={''}
+            val3={null}
+            >
+            </Card>
+          </div>
+
+          )}
+
+          {Object.values(favV).map((o,i)=>
+          <div key={i} >
+            <Card 
+            key={i} 
+            bgcolor={o.Color}
+
+            //front card
+            title={'Kart'}
+            name={o.Vehicle}
+            img={o.Image}
+            height={'94.72'}
+            width={'148px'}
+
+            //back card
+            cat1={'Speed'}
+            val1={o.Speed}
+            cat2={'Acceleration'}
+            val2={o.Acceleration}
+            cat3={'Mini Turbo'}
+            val3={o.MiniTurbo}
+            />
+          </div>
+          )}
+
+          {Object.values(favW).map((o,i)=>
+          <div  key={i}>
+            <Card 
+
+            bgcolor={o.Color}
+
+            //front card
+            title={'Tires'}
+            name={o.Body}
+            img={o.Image}
+            height={'94.72'}
+            width={'148px'}
+
+            //back card
+            cat1={'Speed'}
+            val1={o.Speed}
+            cat2={'Handling'}
+            val2={o.Handling}
+            cat3={'Traction'}
+            val3={o.Traction}
+            />
+          </div>
+          )}
+
+          {Object.values(favG).map((o,i)=>
+          <div key={i}>
+            <Card
+            key={i}
+            bgcolor={o.Color}
+
+
+            //front card
+            title='Glider'
+            name={o.Body}
+            img={o.Image}
+            height={'94.72'}
+            width={'148px'}
+
+            //back card
+            cat1={'Type'} 
+            val1={o.Type}
+            cat2={'Weight'}
+            val2={o.Weight}
+            cat3={'Air Speed'}
+            val3={o.SpeedAir}
+            />
+          </div>
+          )}
+        </CardCont>  
     
-      )}
-        {Object.values(favV).map((o,i)=>
-      <Card 
-      key={i} 
-      bgcolor={o.Color}
-
-      //front card
-      title={'Kart'}
-      name={o.Vehicle}
-      img={o.Image}
-      height={'94.72'}
-      width={'148px'}
-
-      //back card
-      cat1={'Speed'}
-      val1={o.Speed}
-      cat2={'Acceleration'}
-      val2={o.Acceleration}
-      cat3={'Mini Turbo'}
-      val3={o.MiniTurbo}
-    />
-    
-      )}
-        {Object.values(favW).map((o,i)=>
-      <Card 
-      key={i}
-      bgcolor={o.Color}
-
-      //front card
-      title={'Tires'}
-      name={o.Body}
-      img={o.Image}
-      height={'94.72'}
-      width={'148px'}
-      
-      //back card
-      cat1={'Speed'}
-      val1={o.Speed}
-      cat2={'Handling'}
-      val2={o.Handling}
-      cat3={'Traction'}
-      val3={o.Traction}
-    />
-    
-      )}
-        {Object.values(favG).map((o,i)=>
-      <Card
-      key={i}
-      bgcolor={o.Color}
-
-
-      //front card
-      title='Glider'
-      name={o.Body}
-      img={o.Image}
-      height={'94.72'}
-      width={'148px'}
-
-      //back card
-      cat1={'Type'} 
-      val1={o.Type}
-      cat2={'Weight'}
-      val2={o.Weight}
-      cat3={'Air Speed'}
-      val3={o.SpeedAir}
-    />
-    
-      )}
-    </CardCont>  
-  
-     <BottomBarCont>
-      <BottomBar nextlink='/'/>
-    </BottomBarCont>
+      <BottomBarCont>
+        <BottomBar nextlink='/'/>
+      </BottomBarCont>
     </Container>
   )
 }
